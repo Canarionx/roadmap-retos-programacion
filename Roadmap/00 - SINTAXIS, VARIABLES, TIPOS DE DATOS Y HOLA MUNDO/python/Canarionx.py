c0605403c838353bf689b2f73932563c3e1d9052
Canarionx <167861726+Canarionx@users.noreply.github.com>
2024-04-23 12:30:16 +0100

    Canarionx.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Canarionx.py
@@ -0,0 +1,52 @@
+ # El Lenguaje que he seleccionado: https://www.python.org
+
+ # Comentario en una linea y en varias lineas
+ # ------------------------------------------*
+
+"""
+Esto tambien puede
+ser un comentario
+en varias lineas
+"""
+
+#Comentario en varias lineas comillas simple
+#-------------------------------------------*
+
+'''
+Esto tambien puede
+ser un comentario
+en varias lineas
+'''
+
+ # Las variables y Constantes
+ # ----------------------------*
+
+my_variable = "Python"
+
+# En Python no existen las constantes
+# -----------------------------------*   
+
+MY_CONSTANT = "Mi contaste"  # por convención
+
+# Variable para numeros enteros
+# -------------------------------*
+
+my_int = 1
+
+# Variable para numeros decimales 
+# -------------------------------*
+
+my_float = 1.5
+
+# Variable para ser Verdadero o Falso
+# -------------------------------* 
+my_bool = True
+my_bool = False
+
+#Cadena de Texto
+my_string = "Mi cadena de texto"
+
+#Cadena de Texto comillas simple
+my_other_string = 'Mi cadena de texto'
+
+print("¡Holaa Chavales!")
